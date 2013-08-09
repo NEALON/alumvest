5502449d764c63d7ff890976fbd5f9f48fccb82b
Kyle Sun <kyle@bigkyle.com>
2013-08-08 22:02:17 -0700

    update user name and email during sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
 
   attr_accessible :provider, :uid, :name, :first_name, :middle_name, :last_name, :gender, :date_of_birth, :email, :facebook, :linkedin, :mobile_phone, :home_phone, :address_1, :address_2, :city, :state, :zipcode, :personal_statement
 
-
   has_many :identities
 
   def self.from_omniauth(auth)
@@ -16,6 +15,8 @@ class User < ActiveRecord::Base
       user.uid = auth["uid"]
       user.first_name = auth["info"]["first_name"]
       user.last_name = auth["info"]["last_name"]
+      user.name = user.first_name + " " + user.last_name
+      user.email = auth["info"]["email"]
     end
   end
 end
