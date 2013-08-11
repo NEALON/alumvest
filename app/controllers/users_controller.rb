ac2702d97f9463eb2333b304df9153ed2d61d6c6
Kyle Sun <kyle@bigkyle.com>
2013-08-11 01:46:00 -0700

    fix show user profile when user not login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by_id(params[:id])
-    if @user.id == @current_user.id
+    if @current_user != nil and @user.id == @current_user.id
       @user.update_attributes(params[:user])
       #@user.investor.update_attributes(params[:investor]) if @user.is_investor?
       #@user.owner.update_attributes(params[:owner]) if @user.is_owner?
