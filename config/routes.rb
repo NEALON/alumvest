7f22c0331375b290c0e179ab8a8ee95a5a6ce174
Kyle Sun <kyle@bigkyle.com>
2013-08-11 01:56:02 -0700

    clean up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,7 @@ Av::Application.routes.draw do
   resources :company_funding_doc_forms
   resources :company_market_forms
   resources :company_personnel_forms
-
   resources :users
-  post '/users/:id', to: 'users#update'
 
   ### Begin RailsThemes basic generated routes ###
 
