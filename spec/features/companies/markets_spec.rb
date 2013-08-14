5c6034244a59e0632cbacc470f1ae46c199691df
Mike Pence <mike.pence@gmail.com>
2013-08-14 16:15:42 -0400

    moving some tests around

--- spec/features/companies/markets_spec.rb
+++ spec/features/companies/markets_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "company funding docs management", :type => :feature do
 
--- spec/features/companies/personnel_spec.rb
+++ spec/features/companies/personnel_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "company personnel management", :type => :feature do
 
