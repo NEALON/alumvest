792ac92a8ca96c0e597e1f4dfd7dc63a844e487e
Mike Pence <mike.pence@gmail.com>
2013-08-18 12:01:58 -0400

    campaign home and subsection displays

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -17,18 +17,49 @@ class CampaignsController < ApplicationController
   def show
     @company_worksheet = CompanyWorksheet.find(params[:company_worksheet_id])
     @campaign = Campaign.find(params[:id])
+    @user = @campaign.company_worksheet.owner.user
   end
 
   def edit
     @company_worksheet = CompanyWorksheet.find(params[:company_worksheet_id])
-    @campaign = Campaign.new
+    @campaign = Campaign.find(params[:id])
+    render :action => :new
   end
 
   def update
+    @company_worksheet = CompanyWorksheet.find(params[:company_worksheet_id])
+    @campaign = Campaign.find(params[:id])
+    @campaign.update_attributes(params[:campaign])
+    if @campaign.valid?
+      redirect_to company_worksheet_campaign_path(@company_worksheet, @campaign), :notice => 'Campaign info saved.'
+    else
+      render :edit
+    end
+  end
 
+  def team
+    load_stuff_for_subsections
+  end
+
+  def investment_term
+    load_stuff_for_subsections
+  end
+
+  def updates
+    load_stuff_for_subsections
+  end
+
+  def join_us
+    load_stuff_for_subsections
   end
 
   private
+
+  def load_stuff_for_subsections
+    @company_worksheet = CompanyWorksheet.find(params[:company_worksheet_id])
+    @campaign = Campaign.find(params[:campaign_id])
+    @user = @campaign.company_worksheet.owner.user
+  end
 
   def add_images_if_test!(params)
     if Rails.env.test?
--- app/models/user.rb
+++ app/models/user.rb
@@ -47,4 +47,8 @@ class User < ActiveRecord::Base
   def is_owner?
     user_type == "owner"
   end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
