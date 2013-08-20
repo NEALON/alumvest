0bada275076b163b0a76d601edca4a339782034b
Mike Pence <mike.pence@gmail.com>
2013-08-20 00:10:11 -0400

    Implemented jobscore, added more sections to screenshot functionality, removed some already-used data elements from Other

--- spec/features/workflows/everything_spec.rb
+++ spec/features/workflows/everything_spec.rb
@@ -9,19 +9,48 @@ describe "doing everything", :type => :feature do
   end
 
   it "by creating and editing data" do
-    sign_up :owner
-    # create
+    #sign_up :owner
+    ## create
+    #screenshot_and_save_page if $do_screenshots
+    #click_button 'Manage your company profile'
+    #fill_in_company_basic_form
+    #click_link 'People'
+    #fill_in_company_personnel_form
+    #click_link 'Funding Docs'
+    #fill_in_company_funding_doc_form
+    #click_link 'Market'
+    #fill_in_company_market_form
+    #click_link 'New campaign'
+    #fill_in_campaign
+    #screenshot_and_save_page if $do_screenshots
+    #click_link "Team"
+    #sleep 1
+    #screenshot_and_save_page if $do_screenshots
+    #
+    #click_link "Investment Terms"
+    #sleep 1
+    #screenshot_and_save_page if $do_screenshots
+    #
+    #click_link "Updates"
+    #screenshot_and_save_page if $do_screenshots
+    #
+    #click_link "Discussion"
+    #sleep 10
+    #screenshot_and_save_page if $do_screenshots
+    #
+    #click_link "Join Us"
+    #sleep 4
+    #screenshot_and_save_page if $do_screenshots
+    #
+    #click_link "Other"
+    #sleep 1
+    #screenshot_and_save_page if $do_screenshots
+    #
+    #visit '/'
+    #screenshot_and_save_page if $do_screenshots
+    #
+    visit "http://www.jobscore.com/jobs/haydukelabsllc/sushi-chef/d81icicuyr44z7iGakhP3Q?ref=rss&sid=68"
     screenshot_and_save_page if $do_screenshots
-    click_button 'Manage your company profile'
-    fill_in_company_basic_form
-    click_link 'People'
-    fill_in_company_personnel_form
-    click_link 'Funding Docs'
-    fill_in_company_funding_doc_form
-    click_link 'Market'
-    fill_in_company_market_form
-    click_link 'New campaign'
-    fill_in_campaign
-    screenshot_and_save_page if $do_screenshots
+
   end
 end
