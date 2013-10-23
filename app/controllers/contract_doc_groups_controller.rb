3ea64ac7d08bf662c755363061b7aae5c6c510d1
Mike Pence <mike.pence@gmail.com>
2013-10-23 11:00:25 -0400

    embedding a docusign document

--- app/controllers/contract_doc_groups_controller.rb
+++ app/controllers/contract_doc_groups_controller.rb
@@ -16,12 +16,15 @@ class ContractDocGroupsController < ApplicationController
     redirect_to campaign_investment_finalization_path(@campaign, @investment_finalization), :flash => {:success => 'Item completed.'}
   end
 
-
   def show
     @campaign = Campaign.find(params[:campaign_id])
     @company = @campaign.company
     @investment_finalization = InvestmentFinalization.find(params[:investment_finalization_id])
     @contract_doc_group = @investment_finalization.contract_doc_group
+
+    template = DocusignTemplate.find_or_create_by_template_id('04A23610-C1A5-4DDA-8339-7690DFDF92F0')
+    envelope = template.create_envelope({:name => 'Mike Pence', :email => 'mike.pence@gmail.com'})
+    @signing_url = envelope.get_recipient_view
   end
 end
 
