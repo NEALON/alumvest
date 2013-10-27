2cf425412e93b0739f54d3e530aa6f5445135006
Mike Pence <mike.pence@gmail.com>
2013-10-27 13:02:19 -0400

    Re-direct back to contract section after logging envelope event on callback from Docusign

--- app/controllers/docusign_envelopes_controller.rb
+++ app/controllers/docusign_envelopes_controller.rb
@@ -3,13 +3,10 @@ class DocusignEnvelopesController < ApplicationController
   def record_event
     envelope = DocusignEnvelope.find_by_envelope_id(params[:docusign_envelope_id])
     unless envelope.blank?
-      #possible events:
-      #signing_complete, declined, cancel
+      # possible events: signing_complete, declined, cancel
       envelope.events << DocusignEnvelopeEvent.create(:status => params[:event], :status_date_time => Time.now)
-      # TODO, update related entities and re-direct to next step in the workflow
-      redirect_to root_path, :flash => {:success => "Super!"}
+      redirect_to campaign_investment_finalization_contract_doc_group(envelope.signing.investment_finalization), :flash => {:success => "Document signing status has been updated."}
     else
-      # TODO: some logging
       redirect_to root_path, :flash => {:warning => "Could not update envelope."}
     end
   end
