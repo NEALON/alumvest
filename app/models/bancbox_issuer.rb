01d6e8844d9d52ed7e48fcbd1f98dfbf70d08192
Kyle Sun <kyle@alumvest.com>
2013-11-17 00:43:48 -0800

    Validate company fields (all or nothing)

--- app/models/bancbox_issuer.rb
+++ app/models/bancbox_issuer.rb
@@ -2,7 +2,22 @@ class BancboxIssuer < BancboxPersonBase
 
   attr_accessible :company_name, :company_type, :company_registration_state, :company_tax_id
 
+  validate :validates_company_fields
+
   belongs_to :user
+
+  def validates_company_fields
+    # all or nothing for company infos
+    unless [
+        self.company_name.blank?,
+        self.company_registration_state.blank?,
+        self.company_tax_id.blank?
+    ].uniq.length == 1
+      errors.add(:company_name, "can't be blank") if self.company_name.blank?
+      errors.add(:company_registration_state, "can't be blank") if self.company_registration_state.blank?
+      errors.add(:company_tax_id, "can't be blank") if self.company_tax_id.blank?
+    end
+  end
 
   def submit!
     super do |common_options|
