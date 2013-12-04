f96e80f0f6e4b743b5b343d6e087caca3fd50daa
Mike Pence <mike.pence@gmail.com>
2013-12-04 11:46:37 -0500

    display raise summary for investor payment entry in investment finalizations

--- spec/factories/investment_terms.rb
+++ spec/factories/investment_terms.rb
@@ -1,14 +1,16 @@
 FactoryGirl.define do
   factory :investment_term do
-    fundraising_amount                1000
-    campaign_length                   90
-    funds_retained                    'All-or-nothing'
-    fees_paid_by                      "Owner"
-    elevator_pitch                    "#{Faker::Lorem.sentence(10)} #{Faker::Lorem.sentence(10)}"
-    investment_details                "#{Faker::Lorem.sentence(10)} #{Faker::Lorem.sentence(10)}"
-    equity_type                       'Convertible Note'
-    business_plan_url                 Faker::Internet.url
-    financial_statement_url           Faker::Internet.url
-    term_sheet_url                    Faker::Internet.url
+    fundraising_amount      10000
+    min_investment          100
+    max_investment          10000
+    campaign_length         90
+    funds_retained          'All-or-nothing'
+    fees_paid_by            'Owner'
+    elevator_pitch          "#{Faker::Lorem.sentence(10)} #{Faker::Lorem.sentence(10)}"
+    investment_details      "#{Faker::Lorem.sentence(10)} #{Faker::Lorem.sentence(10)}"
+    equity_type             'Convertible Note'
+    business_plan_url       Faker::Internet.url
+    financial_statement_url Faker::Internet.url
+    term_sheet_url          Faker::Internet.url
   end
 end
