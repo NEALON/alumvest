efbcceec70ceaba20ccabdb859d3d69fbaa10c98
Kyle Sun <kyle@alumvest.com>
2013-12-08 10:55:41 -0800

    add investor spec

---
+++ spec/factories/bancbox/issuers.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :bancbox_issuer, :class => Bancbox::Issuer do
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    phone Faker::Number.number(10)
+    email Faker::Internet.email
+    address_1 Faker::Address.street_address
+    city Faker::Address.city
+    state Faker::Address.state_abbr
+    zip Faker::Address.zip_code
+    agreement true
+    #company_name Faker::Name.first_name
+    #company_type 'LLC'
+    #company_registration_state Faker::Address.state_abbr
+    #company_tax_id Faker::Number.number(9)
+  end
+end
---
+++ spec/models/bancbox/issuer_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper_without_capybara'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'vcr_cassettes'
+  c.hook_into :webmock
+end
+
+describe Bancbox::Issuer do
+  before :each do
+    VCR.use_cassette('bancbox', :match_requests_on => [:method, :uri], :record => :new_episodes) do
+      @user = FactoryGirl.create(:user)
+      @issuer = FactoryGirl.create(:bancbox_issuer)
+      @bank_account = FactoryGirl.create(:bancbox_bank_account)
+      @issuer.user = @user
+      @issuer.should be_valid
+      @bank_account.issuer = @issuer
+      @bank_account.should be_valid
+      @issuer.submit!(@bank_account)
+    end
+  end
+
+  it "can create a bancbox issuer" do
+    @issuer.submitted?.should == true
+    @issuer.bancbox_id.should_not == nil
+    @issuer.bank_name.should_not == nil
+    @issuer.account_number.should_not == nil
+    @issuer.account_routing_number.should_not == nil
+    @issuer.account_type.should_not == nil
+    @issuer.issuer_bank_accounts.size.should == 1
+  end
+
+  it "can link another external bank account" do
+    @issuer.issuer_bank_accounts.size.should == 1
+    VCR.use_cassette('bancbox', :match_requests_on => [:method, :uri], :record => :new_episodes) do
+      another_bank_account = FactoryGirl.create(:bancbox_bank_account)
+      another_bank_account.issuer = @issuer
+      another_bank_account.should be_valid
+
+      @issuer.issuer_bank_accounts.size.should == 1
+      @issuer.link_bank_account(:issuer, another_bank_account)
+      @issuer.issuer_bank_accounts.size.should == 2
+    end
+  end
+end
