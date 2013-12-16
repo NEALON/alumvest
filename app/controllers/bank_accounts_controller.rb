8fad2d049ce17d9dd332efca51b95dbbd442996d
Mike Pence <mike.pence@gmail.com>
2013-12-16 17:48:10 -0500

    Require a complete user profile to manage bank account information. Display bancbox error in flash message if cannot successfully create an account. Allow updating bank account information to re-try bancbox account creation.

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -3,18 +3,23 @@ class BankAccountsController < ApplicationController
   def new
     @user = User.find_by_id(params[:user_id])
     @active = 'bank_account'
-    @bank_account = Bancbox::BankAccount.new(
-        :user => @user,
-        :bank_account_holder => current_user.name,
-        :bank_account_routing => Bancbox::BankAccount::DefaultRoutingNumber)
-    render :layout => "users"
+
+    if @user.profile_complete?
+      @bank_account = Bancbox::BankAccount.new(
+          :user => @user,
+          :bank_account_holder => current_user.name,
+          :bank_account_routing => Bancbox::BankAccount::DefaultRoutingNumber)
+      render :layout => "users"
+    else
+      render 'show_profile_incomplete', :layout => 'users'
+    end
   end
 
   def create
     @user = User.find_by_id(params[:user_id])
     @bank_account = Bancbox::BankAccount.create(params[:bancbox_bank_account])
     if @bank_account.valid?
-      redirect_to user_bank_account_path(@user), :flash => {:sucess => 'Bank account info saved.'}
+      create_bancbox_account
     else
       render :action => :new
     end
@@ -39,9 +44,24 @@ class BankAccountsController < ApplicationController
     @bank_account = @user.bank_account
     @bank_account.update_attributes(params[:bancbox_bank_account])
     if @bank_account.valid?
-      redirect_to user_bank_account_path(@user), :flash => {:sucess => 'Bank account info saved.'}
+      unless @user.investor.bancbox_investor.has_bancbox_account?
+        create_bancbox_account
+      else
+        redirect_to user_bank_account_path(@user), :flash => {:sucess => 'Your bank account information was saved.'}
+      end
     else
       render :action => :edit
+    end
+  end
+
+  private
+
+  def create_bancbox_account
+    result = BancboxAccountManager.banking_account_updated!(@user, @bank_account)
+    unless result.class == BancBoxCrowd::Error
+      redirect_to user_bank_account_path(@user), :flash => {:sucess => 'Your bank account information was saved.'}
+    else
+      redirect_to user_bank_account_path(@user), :flash => {:danger => "Bancbox error: #{eval(result.message)['message']}"}
     end
   end
 end
--- spec/features/users/bank_accounts_spec.rb
+++ spec/features/users/bank_accounts_spec.rb
@@ -4,24 +4,25 @@ describe 'users bank accounts', :type => :feature do
 
   before :each do
     sign_up :investor
+    fill_in_user_basic_info
     click_on 'Bank Account'
   end
 
   it 'creates' do
     fill_in_user_bank_account
-    expect(page).to have_content 'Bank account info saved.'
+    expect(page).to have_content 'Your bank account information was saved.'
   end
 
   it 'updates' do
     fill_in_user_bank_account
     click_on 'Edit'
     fill_in_user_bank_account
-    expect(page).to have_content 'Bank account info saved.'
+    expect(page).to have_content 'Your bank account information was saved.'
   end
 
   it 'validates' do
     click_button 'Save'
-    expect(page).to have_content('errors')
+    expect(page).to have_content('error')
   end
 
   it 'makes a bancbox account when the profile is complete' do
