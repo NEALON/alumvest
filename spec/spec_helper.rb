5f1f645be4fd992da00bc790724c19b1255f64c4
Kyle Sun <kyle@alumvest.com>
2013-12-18 00:06:31 -0800

    fixing webmock conflict with rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,11 @@ require 'email_spec'
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 require 'factory_girl_rails'
+require 'vcr'
+
+VCR.configure do |c|
+  c.allow_http_connections_when_no_cassette = true
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
