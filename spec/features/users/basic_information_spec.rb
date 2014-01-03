a87b8488c376c6bd2dc01a3e7ea912830c7e56f0
Kyle Sun <kyle@alumvest.com>
2014-01-03 01:06:30 -0800

    fix basic_information_spec

--- spec/features/users/basic_information_spec.rb
+++ spec/features/users/basic_information_spec.rb
@@ -7,14 +7,12 @@ describe 'users basic information', :type => :feature do
   end
 
   it 'updates' do
-    click_on 'Edit'
     fill_in_user_basic_info
     expect(page).to have_content 'info saved'
   end
 
   it 'validates required fields' do
-    click_on 'Edit'
     click_on 'Save'
-    expect(page).to have_content '8 errors encountered'
+    expect(page).to have_content '6 errors encountered'
   end
 end
