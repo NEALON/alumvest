278da82c2fbc1aa9509945395da820f50b9652eb
Mike Pence <mike.pence@gmail.com>
2014-02-16 13:27:08 -0500

    Keep issuer campaign a has-one

--- app/models/alumvest/issuer/data.rb
+++ app/models/alumvest/issuer/data.rb
@@ -11,7 +11,7 @@ module Alumvest::Issuer::Data
 
     belongs_to :user, :class_name => 'Alumvest::User::Base'
 
-    has_many :campaigns,
+    has_one :campaign,
             :class_name => 'Alumvest::Campaign::Base',
             :foreign_key => 'issuer_id'
     has_one :bancbox_issuer,
