ee30d49a51e57203c822e3a8b35c81e493522cec
Mike Pence <mike.pence@gmail.com>
2014-03-06 17:32:57 -0500

    make accredited investor permissions include self-accredited investor permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,7 +17,7 @@ class Ability
 
       # issuer
       if user.is_issuer?
-        can :manage,IssuerBase
+        can :manage, IssuerBase
 
         can :manage, CampaignBase, :issuer_id => user.issuer.id
         can :view_risk_n_challenge, CampaignBase, :issuer_id => user.issuer.id
@@ -28,22 +28,21 @@ class Ability
       end
 
       if user.is_investor?
-        can :manage,InvestorBase
+        can :manage, InvestorBase
         can :manage, InvestmentBase
         can :manage, Alumvest::SelfAccreditedStatus, :investor_id => user.investor.id
         can :manage, Veritax::Order::Base, :investor_id => user.investor.id
 
-        if user.is_self_accredited_investor?
+        if user.is_self_accredited_investor? or user.is_accredited_investor?
           can :view_risk_n_challenge, CampaignBase
           can :view_exact_funding_amount, CampaignBase
           can :invest, CampaignBase
-        end
 
-        # this is a superset of self_accredited_investor
-        if user.is_accredited_investor?
-          can :view_financial, CampaignBase
-          can :view_pitch_deck, CampaignBase
-          can :view_investment_doc, CampaignBase
+          if user.is_accredited_investor?
+            can :view_financial, CampaignBase
+            can :view_pitch_deck, CampaignBase
+            can :view_investment_doc, CampaignBase
+          end
         end
 
         # admin can do anything
@@ -55,29 +54,29 @@ class Ability
   end
 end
 
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= UserBase.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+# Define abilities for the passed in user here. For example:
+#
+#   user ||= UserBase.new # guest user (not logged in)
+#   if user.admin?
+#     can :manage, :all
+#   else
+#     can :read, :all
+#   end
+#
+# The first argument to `can` is the action you are giving the user
+# permission to do.
+# If you pass :manage it will apply to every action. Other common actions
+# here are :read, :create, :update and :destroy.
+#
+# The second argument is the resource the user can perform the action on.
+# If you pass :all it will apply to every resource. Otherwise pass a Ruby
+# class of the resource.
+#
+# The third argument is an optional hash of conditions to further filter the
+# objects.
+# For example, here the user can only update published articles.
+#
+#   can :update, Article, :published => true
+#
+# See the wiki for details:
+# https://github.com/ryanb/cancan/wiki/Defining-Abilities
