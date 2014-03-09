ad97fb37a3c7773c81c94e5f8947253e6da82018
Mike Pence <mike.pence@gmail.com>
2014-03-09 12:25:54 -0400

    support for declining and postponing docusign

--- spec/features/users/investor/accredited/signs_documents_spec.rb
+++ spec/features/users/investor/accredited/signs_documents_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'accredited investor signs documents', :type => :feature do
+describe 'accredited investor signing documents', :type => :feature do
 
   before :each do
     create_issuer
@@ -10,14 +10,14 @@ describe 'accredited investor signs documents', :type => :feature do
     @investment = create_investment(@campaign, @user.investor)
   end
 
-  it 'views an important document' do
+  it 'views' do
     visit campaign_investment_path(@campaign, @investment)
     click_on 'Documents'
     click_on 'Review and Sign'
     expect(page).to have_content('Request for Signature')
   end
 
-  it 'signs the document' do
+  it 'signs' do
     visit campaign_investment_path(@campaign, @investment)
     click_on 'Documents'
     click_on 'Review and Sign'
@@ -25,5 +25,34 @@ describe 'accredited investor signs documents', :type => :feature do
     click_on 'Sign Here'
     page.driver.browser.execute_script("$('.Btnyellow_reskin').click();")
     expect(page).to have_content('Document signing status has been updated.')
+    expect(page).to have_content('under review')
   end
+
+  it 'declines' do
+    visit campaign_investment_path(@campaign, @investment)
+    click_on 'Documents'
+    click_on 'Review and Sign'
+    click_on 'Decline'
+    fill_in 'txtDeclineReason', :with => 'Nevermind'
+    click_on 'ds_hldrBdy_dlgDecline_btnDecline_btnInline'
+    expect(page).to have_content('Document signing status has been updated.')
+    expect(page).to have_content('declined')
+  end
+
+  it 'postpones' do
+    visit campaign_investment_path(@campaign, @investment)
+    click_on 'Documents'
+    click_on 'Review and Sign'
+    click_on 'Finish Later'
+    expect(page).to have_content('canceled')
+  end
+
+  #it 'signs on paper' do
+  #  visit campaign_investment_path(@campaign, @investment)
+  #  click_on 'Documents'
+  #  click_on 'Review and Sign'
+  #  click_on 'Sign on Paper'
+  #  ## click_on 'Print and Fax'
+  #  ## expect(page).to have_content('...')
+  #end
 end
