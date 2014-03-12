d1fd748f5740c83f810c10360cc98f83e1437bf5
Mike Pence <mike.pence@gmail.com>
2014-03-12 16:13:26 -0400

    trying production bancbox credentials

--- config/initializers/banc_box_crowd.rb
+++ config/initializers/banc_box_crowd.rb
@@ -1,7 +1,11 @@
 require 'banc_box_crowd'
 
 BancBoxCrowd.configure do |config|
-  config.base_url = "https://sandbox-api.bancboxcrowd.com/crowd/v0/cfp/"
+  if Rails.env.production?
+    config.base_url = "https://api.bancboxcrowd.com/crowd/v0/cfp/"
+  else
+    config.base_url = "https://sandbox-api.bancboxcrowd.com/crowd/v0/cfp/"
+  end
   config.api_key = "eadcf103-8150-494d-b956-8ea78b7eac03"
   config.api_secret = "7a28c43ccb"
 end
