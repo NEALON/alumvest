b85e336bb35fa3bada51c966a3912d99caafdb20
Kyle Sun <kyle@alumvest.com>
2014-04-13 10:44:39 -0700

    comment out ignore exception override

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,14 @@
 Airbrake.configure do |config|
   config.api_key = 'da9b12e40cd239ca5a53c97af497c3a5'
-  config.ignore_only = []
+
+  # Default ignore exception classes:
+  #  ActiveRecord::RecordNotFound
+  #  ActionController::RoutingError
+  #  ActionController::InvalidAuthenticityToken
+  #  CGI::Session::CookieStore::TamperedWithCookie
+  #  ActionController::UnknownHttpMethod
+  #  ActionController::UnknownAction
+  #  AbstractController::ActionNotFound
+  #  Mongoid::Errors::DocumentNotFound
+  #config.ignore_only = []
 end
